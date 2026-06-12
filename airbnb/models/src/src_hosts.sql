8a1ad73f75b91098b7d3e759d37c56ccf40579b0
giangiscoding <giang.notctilth2004@gmail.com>
2026-06-12 17:24:26 +0700

    update

---
+++ airbnb/models/src/src_hosts.sql
@@ -0,0 +1,11 @@
+WITH raw_hosts AS(
+    SELECT * FROM AIRBNB.RAW.RAW_HOSTS
+)
+SELECT
+    ID as host_id,
+    NAME AS host_name,
+    IS_SUPERHOST,
+    CREATED_AT,
+    UPDATED_AT
+FROM 
+    raw_hosts
